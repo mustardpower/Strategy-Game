45d398f89478dd8b16ef5feab4da932a2057bc70
mustardpower <paul_gothard93@hotmail.co.uk>
2017-10-29 10:49:51 +0000

    Fix bug by refactoring so only the text renderer controls lifetime of font objects

--- Global Domination/SDL_TextRenderer.h
+++ Global Domination/SDL_TextRenderer.h
@@ -9,6 +9,7 @@ namespace global_domination
 	namespace text_renderer
 	{
 		TTF_Font* getFont();
+		void getTextDimensions(std::string text, int &w, int &h);
 		void renderText(SDL_Window* window, std::string text, SDL_Rect text_location, SDL_Color foreground_color);
 	};
 }
