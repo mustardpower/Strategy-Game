f0a8f34d96788affc561626b0e4c8ebf2e7fe57a
mustardpower <paul_gothard93@hotmail.co.uk>
2017-11-20 18:43:11 +0000

    Fix list box up / down arrow boundaries
    Can go up but not past the first message. Can go down until the last
    visible item is the top item.

--- Global Domination/SDLListBox.h
+++ Global Domination/SDLListBox.h
@@ -21,20 +21,29 @@ namespace global_domination
 		void clearItems();
 		bool containsPoint(SDL_Rect aRect, int x, int y);
 		void drawSliderBar(SDL_Renderer* renderer);
+		void drawSliderBarUpArrow(SDL_Renderer* renderer);
+		void drawSliderBarDownArrow(SDL_Renderer* renderer);
 		bool handleClick(int x, int y);
 		void nextItem();
 		void previousItem();
 		void render(SDL_Renderer* renderer);
 		void selectCurrentItem();
 		T* selectedItem();
+		SDL_Rect sliderBarClientArea();
+		SDL_Rect sliderBarDownArrowClientArea();
+		SDL_Rect sliderBarUpArrowClientArea();
 		SDL_Rect textLocationForIndex(const int item_index);
 	private:
-		SDL_Rect client_area_;
-		int selected_item_index_;
-		SDL_Window* parent_window_;
-		std::vector<ListItem<T>> items_;
-		const int kItemHeight;
 		SDL_Color background_color_;
+		SDL_Rect client_area_;
+		std::vector<ListItem<T>> items_;
+		SDL_Window* parent_window_;
+		int selected_item_index_;
+		int top_visible_index_;
+		const int kItemHeight;
+		// Number of visible items depends on the font size 
+		//so I'm not sure how to dynamically calculate this value
+		const int kNumberOfVisibleItems = 5;
 	};
 
 	template <typename T>
@@ -43,6 +52,7 @@ namespace global_domination
 	{
 		client_area_ = client_area;
 		selected_item_index_ = 0;
+		top_visible_index_ = 0;
 		parent_window_ = parent;
 		background_color_ = SDL_Color{ 115, 115, 115 };
 	}
@@ -82,11 +92,17 @@ namespace global_domination
 	template<class T>
 	inline void SDLListBox<T>::drawSliderBar(SDL_Renderer* renderer)
 	{
-		SDL_Rect slider_bar_client_area{ client_area_.x + (client_area_.w * 0.90), client_area_.y, client_area_.w * 0.1, client_area_.h };
 		SDL_SetRenderDrawColor(renderer, 255, 136, 140, 0xFF);
-		SDL_RenderFillRect(renderer, &slider_bar_client_area);
+		SDL_RenderFillRect(renderer, &sliderBarClientArea());
 
-		SDL_Rect up_arrow_client_area{ slider_bar_client_area.x, slider_bar_client_area.y, slider_bar_client_area.w, slider_bar_client_area.h * 0.06 };
+		drawSliderBarUpArrow(renderer);
+		drawSliderBarDownArrow(renderer);
+	}
+
+	template<class T>
+	inline void SDLListBox<T>::drawSliderBarUpArrow(SDL_Renderer * renderer)
+	{
+		SDL_Rect up_arrow_client_area = sliderBarUpArrowClientArea();
 		SDL_SetRenderDrawColor(renderer, 134, 255, 140, 0xFF);
 		SDL_RenderFillRect(renderer, &up_arrow_client_area);
 
@@ -95,8 +111,12 @@ namespace global_domination
 			up_arrow_client_area.x + (up_arrow_client_area.w / 2), up_arrow_client_area.y,
 			up_arrow_client_area.x + up_arrow_client_area.w, up_arrow_client_area.y + up_arrow_client_area.h,
 			0, 0, 255, 255);
+	}
 
-		SDL_Rect down_arrow_client_area{ slider_bar_client_area.x, slider_bar_client_area.y + (slider_bar_client_area.h * 0.94), slider_bar_client_area.w, slider_bar_client_area.h * 0.06 };
+	template<class T>
+	inline void SDLListBox<T>::drawSliderBarDownArrow(SDL_Renderer * renderer)
+	{
+		SDL_Rect down_arrow_client_area = sliderBarDownArrowClientArea();
 		SDL_SetRenderDrawColor(renderer, 134, 255, 140, 0xFF);
 		SDL_RenderFillRect(renderer, &down_arrow_client_area);
 
@@ -122,6 +142,24 @@ namespace global_domination
 				items_.at(i).invokeAction();
 				return true;
 			}
+		}
+
+		if (containsPoint(sliderBarUpArrowClientArea(), x, y))
+		{
+			if (top_visible_index_ > 0)
+			{
+				top_visible_index_--;
+			}
+			return true;
+		}
+
+		if (containsPoint(sliderBarDownArrowClientArea(), x, y))
+		{
+			if (top_visible_index_ < kNumberOfVisibleItems)
+			{
+				top_visible_index_++;
+			}
+			return true;
 		}
 
 		return false;
@@ -158,17 +196,20 @@ namespace global_domination
 
 		for (std::vector<ListItem<T>>::const_iterator item = items_.cbegin(); item != items_.cend(); item++)
 		{
-			if (index == selected_item_index_)
+			if (index >= top_visible_index_ && (index < top_visible_index_ + kNumberOfVisibleItems))
 			{
-				text_color = { 0,255,0 };
-			}
-			else
-			{
-				text_color = { 255,0,0 };
-			}
+				if (index == selected_item_index_)
+				{
+					text_color = { 0,255,0 };
+				}
+				else
+				{
+					text_color = { 255,0,0 };
+				}
 
-			SDL_Rect text_location = textLocationForIndex(index);
-			global_domination::text_renderer::renderText(parent_window_, item->reportString(), text_location, text_color, 30, background_color_);
+				SDL_Rect text_location = textLocationForIndex(index);
+				global_domination::text_renderer::renderText(parent_window_, item->reportString(), text_location, text_color, 30, background_color_);
+			}
 			index++;
 		}
 	}
@@ -187,14 +228,36 @@ namespace global_domination
 	}
 
 	template<class T>
+	inline SDL_Rect SDLListBox<T>::sliderBarClientArea()
+	{
+		int list_width = client_area_.w * 0.9;
+		int slider_bar_width = client_area_.w * 0.1;
+		return SDL_Rect { client_area_.x + list_width, client_area_.y, slider_bar_width, client_area_.h };
+	}
+
+	template<class T>
+	inline SDL_Rect SDLListBox<T>::sliderBarDownArrowClientArea()
+	{
+		int arrow_height = sliderBarClientArea().h * 0.06;
+		return SDL_Rect { sliderBarClientArea().x, sliderBarClientArea().y + sliderBarClientArea().h - arrow_height, sliderBarClientArea().w, arrow_height };
+	}
+
+	template<class T>
+	inline SDL_Rect SDLListBox<T>::sliderBarUpArrowClientArea()
+	{
+		int arrow_height = sliderBarClientArea().h * 0.06;
+		return SDL_Rect { sliderBarClientArea().x, sliderBarClientArea().y, sliderBarClientArea().w, arrow_height };
+	}
+
+	template<class T>
 	inline SDL_Rect SDLListBox<T>::textLocationForIndex(const int menu_item_index)
 	{
 		int w = 0;
 		int h = 0;
 
-		std::string text = (items_.at(menu_item_index)).reportString();
+		std::string text = items_.at(menu_item_index).reportString();
 		text_renderer::getTextDimensions(text, w, h);
 		
-		return SDL_Rect{ client_area_.x, client_area_.y + (kItemHeight * menu_item_index), w, h };
+		return SDL_Rect{ client_area_.x, client_area_.y + (kItemHeight * (menu_item_index - top_visible_index_)), w, h };
 	}
 }
