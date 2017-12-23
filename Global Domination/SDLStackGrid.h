afc00dcf8e22b42f44269c2f7ccc5cbf8b3e7215
mustardpower <paul_gothard93@hotmail.co.uk>
2017-12-23 09:22:28 +0000

    Selectable items in stack grid

--- Global Domination/SDLStackGrid.h
+++ Global Domination/SDLStackGrid.h
@@ -17,7 +17,10 @@ namespace global_domination
 		void render(SDL_Renderer* renderer);
 	private:
 		SDL_Rect client_area_;
-		std::array<std::array<StackGridItem<T>, Y>, X> items;
+		std::array<std::array<StackGridItem<T>, Y>, X> items_;
+		int selected_item_index_x_;
+		int selected_item_index_y_;
+
 	};
 
 	template <class T, int X, int Y>
@@ -29,18 +32,48 @@ namespace global_domination
 	template <class T, int X, int Y>
 	inline void SDLStackGrid<T, X, Y>::addItem(StackGridItem<T> item, int location_x, int location_y)
 	{
-		items[location_x][location_y] = item;
+		items_[location_x][location_y] = item;
 	}
 
 	template <class T, int X, int Y>
 	inline bool SDLStackGrid<T, X, Y>::containsPoint(SDL_Rect aRect, int x, int y)
 	{
+		if ((x >= aRect.x) && (x <= (aRect.x + aRect.w)))
+		{
+			if ((y >= aRect.y) && (y <= (aRect.y + aRect.h)))
+			{
+				return true;
+			}
+		}
+
 		return false;
 	}
 
 	template <class T, int X, int Y>
 	inline bool SDLStackGrid<T, X, Y>::handleClick(int x, int y)
 	{
+		if (!containsPoint(client_area_, x, y))
+		{
+			return false;
+		}
+
+		const int width_per_item = client_area_.w / X;
+		const int height_per_item = client_area_.h / Y;
+		for (int i = 0; i < items_.size(); i++)
+		{
+			for (int j = 0; j < items_[i].size(); j++)
+			{
+				SDL_Rect text_location = SDL_Rect{ client_area_.x + (i * width_per_item), client_area_.y + (j * height_per_item), width_per_item, height_per_item };
+				if (containsPoint(text_location, x, y))
+				{
+					selected_item_index_x_ = i;
+					selected_item_index_y_ = j;
+					items_[i][j].invokeAction();
+					return true;
+				}
+			}
+		}
+
 		return false;
 	}
 
@@ -50,18 +83,24 @@ namespace global_domination
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
 		SDL_RenderFillRect(renderer, &client_area_);
 
-		for (int i = 0; i < items.size(); i++)
+		for (int i = 0; i < items_.size(); i++)
 		{
-			for (int j = 0; j < items[i].size(); j++)
+			for (int j = 0; j < items_[i].size(); j++)
 			{
-				StackGridItem<TradeResource> item = items[i][j];
+				StackGridItem<TradeResource> item = items_[i][j];
 				int w = 0;
 				int h = 0;
 				text_renderer::getTextDimensions(item.reportString(), w, h);
-				const int width_per_item = client_area_.w / items.size();
-				const int height_per_item = client_area_.h / items[i].size();
+				const int width_per_item = client_area_.w / items_.size();
+				const int height_per_item = client_area_.h / items_[i].size();
+				SDL_Color text_color = ColorPreferences::getPrimaryTextColor();
+				if ((selected_item_index_x_ == i) && (selected_item_index_y_ == j))
+				{
+					text_color = ColorPreferences::getSelectedTextColor();
+				}
+
 				SDL_Rect text_location = SDL_Rect{ client_area_.x + (i * width_per_item) + (int)(0.5 * width_per_item), client_area_.y + (j * height_per_item) + (int)(0.5 * height_per_item), w, h };
-				global_domination::text_renderer::renderText(parent_, item.reportString(), text_location, ColorPreferences::getPrimaryTextColor(), SDL_Color{ 0,0,0,0xFF }, 15);
+				global_domination::text_renderer::renderText(parent_, item.reportString(), text_location, text_color, SDL_Color{ 0,0,0,0xFF }, 15);
 			}
 		}
 	}
