f8f74d4a06ec597aa6cc0cb8d115e25251f5b03f
mustardpower <paul_gothard93@hotmail.co.uk>
2018-01-08 20:28:04 +0000

    add markers along x and y axis

--- Global Domination/SDLGraphPane.cpp
+++ Global Domination/SDLGraphPane.cpp
@@ -4,6 +4,8 @@ namespace global_domination
 {
 	SDLGraphPane::SDLGraphPane(SDL_Window* parent, SDL_Rect client_area) : SDLControl(parent, client_area)
 	{
+		number_of_intervals_x = 10;
+		number_of_intervals_y = 10;
 	}
 
 	void SDLGraphPane::drawAxes(SDL_Renderer * renderer)
@@ -35,6 +37,20 @@ namespace global_domination
 			y_axis_start_x,
 			y_axis_start_y + axis_length_y + extra_axis_overlap
 		);
+
+		// draw markers on x axis
+		const int interval_x = axis_length_x / number_of_intervals_x;
+		for (int marker_x = x_axis_start_x; marker_x < x_axis_start_x + axis_length_x; marker_x += interval_x)
+		{
+			SDL_RenderDrawLine(renderer, marker_x, x_axis_start_y, marker_x, x_axis_start_y + (extra_axis_overlap / 2));
+		}
+
+		// draw markers on y axis
+		const int interval_y = axis_length_y / number_of_intervals_y;
+		for (int marker_y = y_axis_start_y; marker_y < y_axis_start_y + axis_length_y; marker_y += interval_y)
+		{
+			SDL_RenderDrawLine(renderer, y_axis_start_x - (extra_axis_overlap / 2), marker_y, y_axis_start_x, marker_y);
+		}
 	}
 
 	bool SDLGraphPane::handleClick(int mouse_x, int mouse_y)
