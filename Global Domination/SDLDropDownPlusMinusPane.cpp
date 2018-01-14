618111fadc3dbe649a772a15e4cff515d0f5d775
mustardpower <paul_gothard93@hotmail.co.uk>
2018-01-14 14:46:57 +0000

    add actions to increase / decrease list box value

--- Global Domination/SDLDropDownPlusMinusPane.cpp
+++ Global Domination/SDLDropDownPlusMinusPane.cpp
@@ -12,21 +12,21 @@ namespace global_domination
 
 	void SDLDropDownPlusMinusPane::initialize()
 	{
-		SDL_Rect tax_dropdown_client_area{ client_area_.x, client_area_.y, (int)(client_area_.w * 0.8), client_area_.h };
-		std::shared_ptr<SDLDropDownList> tax_drop_down_list = std::make_shared<SDLDropDownList>(parent_, tax_dropdown_client_area);
-		tax_drop_down_list->setFontSize(16);
+		SDL_Rect dropdown_client_area{ client_area_.x, client_area_.y, (int)(client_area_.w * 0.8), client_area_.h };
+		dropdown_list_ = std::make_shared<SDLDropDownList>(parent_, dropdown_client_area);
+		dropdown_list_->setFontSize(16);
 		std::vector<std::string> items{ "0%", "5%", "10%", "15%", "20%" };
-		tax_drop_down_list->setItems(items);
-		addChildControl(tax_drop_down_list);
+		dropdown_list_->setItems(items);
+		addChildControl(dropdown_list_);
 
-		SDL_Rect minus_button_client_area{ client_area_.x + tax_dropdown_client_area.w, client_area_.y, (int)(client_area_.w * 0.1), client_area_.h };
-		std::shared_ptr<Action> minus_action = std::make_shared<Action>();
+		SDL_Rect minus_button_client_area{ client_area_.x + dropdown_client_area.w, client_area_.y, (int)(client_area_.w * 0.1), client_area_.h };
+		std::shared_ptr<Action> minus_action = std::make_shared<Action>(TYPES::ACTION_LIST::DECREASE_LISTBOX_VALUE);
 		std::shared_ptr<SDLButton> minus_button = std::make_shared<SDLButton>(parent_,"-", minus_action, minus_button_client_area);
 		minus_button->setFontSize(16);
 		addChildControl(minus_button);
 
 		SDL_Rect plus_button_client_area{ minus_button_client_area.x + minus_button_client_area.w, client_area_.y, (int)(client_area_.w * 0.1), client_area_.h };
-		std::shared_ptr<Action> plus_action = std::make_shared<Action>();
+		std::shared_ptr<Action> plus_action = std::make_shared<Action>(TYPES::ACTION_LIST::INCREASE_LISTBOX_VALUE);
 		std::shared_ptr<SDLButton> plus_button = std::make_shared<SDLButton>(parent_,"+", plus_action, plus_button_client_area);
 		plus_button->setFontSize(16);
 		addChildControl(plus_button);
@@ -38,6 +38,21 @@ namespace global_domination
 
 	void SDLDropDownPlusMinusPane::respondToAction(Sint32 action)
 	{
+		switch (action)
+		{
+			case TYPES::ACTION_LIST::DECREASE_LISTBOX_VALUE:
+			{
+				
+			}
+			break;
+			case TYPES::ACTION_LIST::INCREASE_LISTBOX_VALUE:
+			{
+
+			}
+			break;
+		default:
+			break;
+		}
 	}
 
 }
--- Global Domination/SDLDropDownPlusMinusPane.h
+++ Global Domination/SDLDropDownPlusMinusPane.h
@@ -2,6 +2,8 @@
 
 #include "SDLCompositePane.h"
 
+#include "SDLDropDownList.h"
+
 namespace global_domination
 {
 	class SDLDropDownPlusMinusPane : public SDLCompositePane
@@ -11,5 +13,7 @@ namespace global_domination
 		void initialize();
 		void setItems(std::vector<std::string> items);
 		void respondToAction(Sint32 action);
+	private:
+		std::shared_ptr<SDLDropDownList> dropdown_list_;
 	};
 }
