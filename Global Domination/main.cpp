5fc278dd741bb2d81acadf3f30a04b13f69fc63e
mustardpower <paul_gothard93@hotmail.co.uk>
2018-05-20 18:19:41 +0100

    remove unnecessary use of pointer

--- Global Domination/main.cpp
+++ Global Domination/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char* argv[])
 {
-	std::shared_ptr<global_domination::Game> global_domination_game = std::make_shared<global_domination::Game>();
-	global_domination_game->initialize();
-	global_domination_game->runGameLoop();
+	global_domination::Game global_domination_game;
+	global_domination_game.initialize();
+	global_domination_game.runGameLoop();
 	return 0;
 }
